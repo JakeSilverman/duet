79caffee1716a8a776393d2eec07a9d31019522a
JakeSilverman <JakeRossSilverman@gmail.com>
2021-05-03 23:03:35 -0400

    cleaner offset analysis

--- srk/src/pmfa.mli
+++ srk/src/pmfa.mli
@@ -2,7 +2,7 @@ open Syntax
 open Chc
 
 val skolemize : 'a context -> 'a formula -> 'a formula * Symbol.Set.t
-val offset_partitioning : 'a context -> 'a formula -> (symbol, int BatUref.uref) Hashtbl.t
+val offset_partitioning : 'a context -> 'a formula -> (symbol, symbol BatUref.uref) Hashtbl.t
 type chcvar = { rel : Relation.t; param : int} 
 val pmfa_chc_offset_partitioning : 'a context -> 'a fp -> (chcvar, chcvar) Hashtbl.t
 val verify_offset_candidates : 'a context -> 'a fp -> (relation, int) Hashtbl.t -> bool
