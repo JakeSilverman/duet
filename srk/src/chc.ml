faec0f41f7179e2d4a04e02f78ce50d0975aba20
JakeSilverman <JakeRossSilverman@gmail.com>
2022-06-09 08:40:32 -0400

    Changed offset analysis algo to not require offsets for edges without rws

--- srk/src/chc.ml
+++ srk/src/chc.ml
@@ -96,7 +96,10 @@ module Fp = struct
   let add_query fp query = { fp with queries = Symbol.Set.add query fp.queries} 
 
   let map_rules map fp = { fp with rules = List.map map fp.rules} 
-  let filter_rules filter fp = { fp with rules = List.filter filter fp.rules} 
+  let filter_rules filter fp = { fp with rules = List.filter filter fp.rules}
+  let filteri_rules filter fp = { fp with rules = List.filteri filter fp.rules}
+  let iteri_rules iter fp = List.iteri iter fp.rules 
+
   let mapi_rules map fp = { fp with rules = List.mapi map fp.rules} 
 
 
