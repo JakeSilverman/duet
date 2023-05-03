dc3fbd9ec646ab13bc8a1264dfe8349328359821
JakeSilverman <JakeRossSilverman@gmail.com>
2023-05-03 10:28:50 -0400

    preparing to resynch branch

--- srk/test/test_srk.ml
+++ srk/test/test_srk.ml
@@ -1,7 +1,7 @@
 open OUnit
 
 let suite = "Main" >::: [
-    Test_memo.suite;
+    (*Test_memo.suite;
     Test_scalar.suite;
     Test_loop.suite;
     Test_fixpoint.suite;
@@ -17,16 +17,15 @@ let suite = "Main" >::: [
     Test_polynomialCone.suite;
     Test_exppolynomial.suite;
     Test_apron.suite;
-    Test_simplify.suite;
+    Test_simplify.suite;*)
     Test_quantifier.suite;
-    Test_wedge.suite;
+    (*Test_wedge.suite;
     Test_abstract.suite;
-    Test_lts.suite;
-    Test_transitionIdeal.suite;
+    Test_lts.suite;*)
     Test_iteration.suite;
-    Test_termination.suite;
-    Test_transition.suite;
-    Test_WeightedGraph.suite;
+    (*Test_termination.suite;*)
+    (*Test_transition.suite;
+    Test_WeightedGraph.suite;*)
 (*    Test_chc.suite;
     Test_pmfa.suite*)
 ]
