501408415bf8fb1bc871c028cd39a153ab751364
jake <jakers@cs.princeton.edu>
2024-07-09 16:43:16 -0400

    Revert "removed most logging statements"
    This reverts commit a064462b5b42b05ce2f8d9e9780ade512f225f02.
    
    undoing last commii

--- srk/src/pmfa.ml
+++ srk/src/pmfa.ml
@@ -181,15 +181,7 @@ module OldPmfa = struct
           | None -> None
         end
       | `Var _ -> None
-      | `App (a, []) ->
-      (* Double check we can just make new var like this*)
-      if Hashtbl.mem rmap a then 
-        Some (mk_const srk (Hashtbl.find rmap a))
-      else (
-         let z = mk_symbol srk ~name:("z"^(show_symbol srk a)) `TyInt in
-         Hashtbl.add rmap a z;
-         Some (mk_const srk (Hashtbl.find rmap a))
-      )
+      | `App (a, []) -> Some (mk_const srk (Hashtbl.find rmap a))
       | `Ite _ -> assert false
       | _ -> assert false
     in
@@ -853,7 +845,7 @@ module OldPmfa = struct
 
       (* this was changed to use pre for map instead of post for term... what
 does this affect *)
-      let eqs_trs =  Symbol.Map.empty (*
+      let eqs_trs = Symbol.Map.empty (*
         List.fold_left (fun eqs_trs (a, b) ->
             if List.mem (a, b) (T.symbols tf) then (
               Symbol.Map.add b a eqs_trs)
@@ -917,10 +909,14 @@ does this affect *)
     let tf_pmfa = T.update_symbols tf_pmfa !trs in
     let proj_ind, arr_map, tf_proj, arr_only_trs, symb_consts = projection_with_store_elim srk tf_pmfa eqs_trs new_eqs_consts in
 
+    Log.errorf "Formula with new proj is %a" (Formula.pp srk) (T.formula tf_proj);
+
+    List.iter (fun (a, b) -> Log.errorf "Symbol is %a and %a" (pp_symbol srk) a (pp_symbol srk) b) (TransitionFormula.symbols tf_proj);
     let tf_proj = T.update_formula tf_proj (eliminate_ite srk (T.formula tf_proj)) in
 
     let tf_proj = T.update_formula tf_proj (eliminate_stores srk (T.formula tf_proj)) in
 
+    Log.errorf "PRIOR TO LIA is %a" (Formula.pp srk) (T.formula tf_proj);
     let lia, _ = pmfa_to_lia srk (T.formula tf_proj) in
 
 
@@ -932,11 +928,13 @@ does this affect *)
 
     let lia = Quantifier.eq_guided_elim_loop srk lia in
 
+    Log.errorf "Phi in abstract is %a" (Formula.pp srk) lia;
 
     let lia, skolems = skolemize_eh_alt srk lia in 
     let lia = Quantifier.miniscope srk lia in
     let ground_lia = Quantifier.mbp_qe_inplace srk lia in
 
+    Log.errorf "Ground is is %a" (Formula.pp srk) ground_lia;
 
 
 
@@ -956,6 +954,7 @@ does this affect *)
 
     let at_most_single_write srk write noop trs =
 
+      Log.errorf "NOOP IS %a" (Formula.pp srk) (T.formula noop);
       let exp = mk_symbol srk ~name:"exp" `TyInt in
 
       let noop_star =
@@ -1019,6 +1018,7 @@ does this affect *)
         rewrite srk ~down:(nnf_rewriter srk) noop
       in
       
+      Log.errorf "write is %a" (Formula.pp srk) write;
       let exists s = not (Symbol.Set.mem s obj.skolems) in
       let write = T.make ~exists write obj.iter_trs in
       let noop = T.make ~exists noop obj.iter_trs in
@@ -1324,6 +1324,7 @@ let phase_mp_ported srk candidate_predicates tf nonterm =
 
 
     let mp srk tf =
+      Log.errorf "FORMULA entry tf is %a" (Formula.pp srk) (T.formula tf);
       (*let sym_to_var = Hashtbl.create 991 in
 
         let of_symbol sym =
@@ -1366,18 +1367,21 @@ let phase_mp_ported srk candidate_predicates tf nonterm =
       let _star = Some star in
       let flatten_trs =
         List.fold_left (fun flat (x, x') ->
+            Log.errorf "Symbol is %a" (pp_symbol srk) x;
             x :: x' :: flat)
           (abs.proj_ind :: abs.symb_consts)
           (abs.iter_trs @ (Symbol.Map.bindings abs.eqs_ints_trs))
       in
+      Log.errorf "Formula reduc is %a" (Formula.pp srk) (T.formula tf_iter);
       let mp_lia = 
         (** over-approximate possibly non-terminating conditions for a transition *)
         begin
           let open Syntax in
           let nonterm tf =
+            Log.errorf "entry nonterm tf is %a" (Formula.pp srk) (T.formula tf);
             let pre =
               let fresh_skolem =
-                Memo.memo (fun sym -> 
+                Memo.memo (fun sym -> Log.errorf "Dupping sym %a" (pp_symbol srk) sym;
                             mk_const srk (dup_symbol srk sym))
               in
               let subst sym =
@@ -1387,6 +1391,7 @@ let phase_mp_ported srk candidate_predicates tf nonterm =
               in
               substitute_const srk subst (T.formula tf)
             in
+            Log.errorf "pre is %a" (Formula.pp srk) pre;
             let llrf, has_llrf =
               if !termination_llrf then
                 if TLLRF.has_llrf srk tf then
@@ -1424,6 +1429,7 @@ let phase_mp_ported srk candidate_predicates tf nonterm =
                 | _ -> [mp]
               else []
             in
+            Log.errorf " exp is %a" (Formula.pp srk) (mk_and srk exp);
             let result =
               Syntax.mk_and srk (llrf@dta@exp)
             in
@@ -1439,6 +1445,7 @@ let phase_mp_ported srk candidate_predicates tf nonterm =
                   let x = mk_const srk x in
                   let x' = mk_const srk x' in
 
+                  Log.errorf "x is %a" (ArithTerm.pp srk) x;
                   [mk_lt srk x x';
                    mk_lt srk x' x;
                    mk_eq srk x x'])
@@ -1453,17 +1460,20 @@ let phase_mp_ported srk candidate_predicates tf nonterm =
       in
 
       let mp_lia =  rewrite srk ~down:(nnf_rewriter srk) mp_lia in
+      Log.errorf "Formula MP LIA here is %a" (Formula.pp srk) mp_lia;
       let mp_lia = 
         mk_exists_consts
           srk
           (fun s -> List.mem s flatten_trs)
           mp_lia
       in 
+      Log.errorf "Formula MP LIA POST EXIST is %a" (Formula.pp srk) mp_lia;
 
       let mp_lia = Quantifier.eq_guided_elim_loop srk mp_lia in
 
 
       let mp_lia = Quantifier.mbp_qe_inplace srk mp_lia in
+      Log.errorf "Formula MP LIA MINI EXIST is %a" (Formula.pp srk) mp_lia;
 
 
 
@@ -1477,6 +1487,7 @@ let phase_mp_ported srk candidate_predicates tf nonterm =
       in
       let substed = substitute_const srk map mp_lia in
       let res = (mk_forall srk `TyInt substed) in
+      Log.errorf "Result after MP is %a" (Formula.pp srk) res;
       res
 
 
