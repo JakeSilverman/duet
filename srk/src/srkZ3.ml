6ddb431c430549a0c4ff085bcf7625a7fcd655e1
JakeSilverman <JakeRossSilverman@gmail.com>
2024-07-31 17:34:36 -0400

    undo global context carry

--- srk/src/srkZ3.ml
+++ srk/src/srkZ3.ml
@@ -27,7 +27,6 @@ type 'a open_expr = [
   | `IsInt of 'a list
 ]
 
-let global_context = Z3.mk_context []
 
 let bool_val x =
   match Z3.Boolean.get_bool_value x with
@@ -677,7 +676,7 @@ let load_smtlib2 ?(context=get_default_context ()) srk str =
          | `Term _ -> invalid_arg "load_smtlib2")
   |> mk_and srk
 
-let load_smtlib2_file ?(context=global_context) srk str =
+let load_smtlib2_file ?(context=get_default_context ()) srk str =
   let z3 = context in
   let ast = Z3.SMT.parse_smtlib2_file z3 str [] [] [] [] in
   let sym_of_decl =
